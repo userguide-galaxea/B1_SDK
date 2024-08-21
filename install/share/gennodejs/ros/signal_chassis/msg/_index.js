277dd9de3812f29ff434ee853fff0dc55f4e147a
galaxea\runze.ge <runze.ge@galaxea.ai>
2024-08-21 18:01:58 +0800

    [V1.0.0] release

---
+++ install/share/gennodejs/ros/signal_chassis/msg/_index.js
@@ -0,0 +1,18 @@
+
+"use strict";
+
+let DrivetrainStamped_r1 = require('./DrivetrainStamped_r1.js');
+let controller_signal = require('./controller_signal.js');
+let controller_signal_stamped = require('./controller_signal_stamped.js');
+let DrivetrainStamped = require('./DrivetrainStamped.js');
+let Drivetrain = require('./Drivetrain.js');
+let Drivetrain_r1 = require('./Drivetrain_r1.js');
+
+module.exports = {
+  DrivetrainStamped_r1: DrivetrainStamped_r1,
+  controller_signal: controller_signal,
+  controller_signal_stamped: controller_signal_stamped,
+  DrivetrainStamped: DrivetrainStamped,
+  Drivetrain: Drivetrain,
+  Drivetrain_r1: Drivetrain_r1,
+};
